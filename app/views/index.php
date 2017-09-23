16ee3b35ddd165f23b569aacc0160ce66846bcca
Mysfer <jpfreitas2001@gmail.com>
2017-09-22 21:49:49 -0300

    Modificação do index

---
+++ app/views/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/public/css/bootstrap.css">
+    <title>Página de cadastro</title>
+</head>
+    <body>
+    <form method="post" action="/salvar">
+ 
+                    <div class="form-group">
+                        <label for="nome">Digite um numero</label>
+                        <input class="form-control" type="text" name="numero" id="numero" value="">
+                    </div>
+                        <button class="btn btn-success btn-lg" type="">Enviar</button>                            
+                        </form> 
+    </body>
+</html>
