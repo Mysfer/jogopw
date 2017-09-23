b53da2ee48fdd0fd61f8e25f9ea333a74f9f0f29
MarrentaxD <jsouzajj27@gmail.com>
2017-09-23 20:57:15 -0300

    Formatação do index

--- app/views/index.php
+++ app/views/index.php
@@ -5,16 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/public/css/bootstrap.css">
-    <title>Página de cadastro</title>
+    <title>Jogo PW</title>
 </head>
     <body>
+    <center>
+    <div class="container">
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <h1>Desafio do Número!</h1>
     <form method="post" action="/salvar">
- 
                     <div class="form-group">
-                        <label for="nome">Digite um numero</label>
+                        <label for="nome">Digite um número</label>
                         <input class="form-control" type="text" name="numero" id="numero" value="">
                     </div>
-                        <button class="btn btn-success btn-lg" type="">Enviar</button>                            
-                        </form> 
+                        <button class="btn btn-lg btn-primary" type="">Enviar</button>                            
+    </form> 
     </body>
+    </center>
+    </div>
 </html>
